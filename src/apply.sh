44bcd642f6e8fc8b49309d3e1ef6a1a77255df7f
Art Navsegda <artnavsegda@gmail.com>
2019-09-11 16:59:49 +0300

    all

--- src/apply.sh
+++ src/apply.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-./$@
-
 # cat
 
-# ./$@ | ssh root@192.168.1.1 stdin2arg /bin/ubus call uci set
+ssh root@192.168.1.1 stdin2arg /bin/ubus call uci set
 
--- src/plug2.c
+++ src/plug2.c
@@ -5,6 +5,9 @@
 #include <readline/history.h>
 #include <wjelement.h>
 #include <wjreader.h>
+#include <wjwriter.h>
+#include <wjreader.h>
+#include <unistd.h>
 #include "filter.h"
 #include "config.h"
 #include "load.h"
@@ -14,6 +17,7 @@ WJElement root = NULL;
 
 int main(int argc, char *argv[])
 {
+  setuid(0);
   root = WJEObject(NULL, NULL, WJE_NEW);
 
   readconfig();
@@ -27,7 +31,17 @@ int main(int argc, char *argv[])
   }
 
   doc = filter(readjson, root, argv[1]);
-  WJEDump(doc);
+
+//  WJWriter writejson;
+
+//  if (!(writejson = WJWOpenFILEDocument(0, stdout))) {
+//    puts("stream failed to open");
+//    return 1;
+//  }
+
+  WJEWriteFILE(doc, stdout);
+
+  //WJEDump(doc);
 
   return 0;
 }
