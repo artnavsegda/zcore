aeea67936ffb1b5e1d39c406b344f8e815166274
Art Navsegda <artnavsegda@gmail.com>
2019-09-12 14:59:29 +0300

    generator done

--- src/generator.c
+++ src/generator.c
@@ -6,38 +6,46 @@
 
 WJElement generator(char * generatedname, WJElement schema, char * schemapath)
 {
+  char * itemname;
   WJElement output = WJEObject(NULL, "generatedname", WJE_NEW);
   WJElement property = NULL;
+  WJElement reqstring = NULL;
 
   WJEString(output,"name",WJE_NEW,generatedname);
 
-  /*while (property = _WJEObject(schema,schemapath, WJE_GET, &property))
+  //WJEDump(schema);
+
+  while (itemname = WJEStringF(schema, WJE_GET, &reqstring, NULL, "%s.required[]", schemapath))
   {
+    //puts(itemname);
+    property = WJEObjectF(schema, WJE_GET, NULL, "ethernet.schema.items.properties.%s", itemname);
+    //WJEDump(property);
+
     if (strcmp(WJEString(property,"type",WJE_GET,"unknown"),"string") == 0)
     {
-      char * stringvalue = WJEString(ifaceinput,property->name,WJE_GET,"");
+      char * stringvalue = WJEString(property,"example",WJE_GET,"");
       if (stringvalue[0])
       {
-        WJEString(ifaceoutput,property->name,WJE_NEW,stringvalue);
+        WJEString(output,property->name,WJE_NEW,stringvalue);
       }
     }
     else if (strcmp(WJEString(property,"type",WJE_GET,"unknown"),"number") == 0)
     {
-      int numbervalue = WJEInt32(ifaceinput,property->name,WJE_GET,-1);
+      int numbervalue = WJEInt32(property,"example",WJE_GET,-1);
       if (numbervalue != -1)
       {
-        WJEInt32(ifaceoutput,property->name,WJE_NEW,numbervalue);
+        WJEInt32(output,property->name,WJE_NEW,numbervalue);
       }
     }
     else if (strcmp(WJEString(property,"type",WJE_GET,"unknown"),"boolean") == 0)
     {
-      int boolval = WJEInt32(ifaceinput,property->name,WJE_GET,-1);
+      int boolval = WJEInt32(property,"example",WJE_GET,-1);
       if (boolval != -1)
       {
-        WJEBool(ifaceoutput,property->name,WJE_NEW,(XplBool)boolval);
+        WJEBool(output,property->name,WJE_NEW,(XplBool)boolval);
       }
     }
-  }*/
+  }
 
   return output;
 }
