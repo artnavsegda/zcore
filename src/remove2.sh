91d6a0a759f7c287dffc77c29e7d9590d100ede6
Art Navsegda <artnavsegda@gmail.com>
2019-09-26 15:50:04 +0300

    all

--- src/remove2.sh
+++ src/remove2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo deleting $1
-
 ssh root@192.168.1.1 ubus call uci delete '"'{\'config\':\'$1\',\'type\': \'$2\', \'match\':{\'name\':\'$3\'}}'"'
+ssh root@192.168.1.1 /etc/init.d/firewall reload
+
