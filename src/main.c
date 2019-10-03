6cf3058107dd2aeb6101536b46387244d1435373
Art Navsegda <artnavsegda@gmail.com>
2019-10-03 15:30:09 +0300

    all

--- src/main.c
+++ src/main.c
@@ -3,42 +3,41 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <wjelement.h>
-#include <wjreader.h>
 #include <unistd.h>
-#include "interpreter.h"
-#include "completion.h"
-#include "config.h"
-#include "load.h"
-#include "acquire.h"
-#include "prompt.h"
+#include <json-c/json.h>
+//#include "interpreter.h"
+//#include "completion.h"
+//#include "config.h"
+//#include "load.h"
+//#include "acquire.h"
+//#include "prompt.h"
 
-WJElement root = NULL;
+json_object * root = NULL;
 char zcore_prompt[255];
 
 int main(int argc, char *argv[])
 {
-  rl_attempted_completion_function = character_name_completion;
+//  rl_attempted_completion_function = character_name_completion;
 
-  root = WJEObject(NULL, NULL, WJE_NEW);
+  //root = WJEObject(NULL, NULL, WJE_NEW);
 
-  readconfig();
-  loadeveryschema(root,config.schemapath);
+//  readconfig();
+//  loadeveryschema(root,config.schemapath);
 
-  acquireall(root);
+//  acquireall(root);
 
 //  WJEDump(root);
 
-  while (1)
-  {
-    generateprompt(zcore_prompt);
-    char * input = readline(zcore_prompt);
-    if (!input)
-      break;
-    add_history(input);
-    interpret(input);
-    free(input);
-  }
+//  while (1)
+//  {
+//    generateprompt(zcore_prompt);
+//    char * input = readline(zcore_prompt);
+//    if (!input)
+//      break;
+//    add_history(input);
+//    interpret(input);
+//    free(input);
+//  }
 
   return 0;
 }
