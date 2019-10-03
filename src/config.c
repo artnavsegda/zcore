4b362214d087800c7e53f41322b0baee86c3b674
Art Navsegda <artnavsegda@gmail.com>
2019-10-03 17:18:11 +0300

    config

--- src/config.c
+++ src/config.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
-#include <wjelement.h>
-#include <wjreader.h>
 #include <string.h>
 #include "config.h"
 
 struct configstruct_t config;
 
-void parseconfig(WJElement configjson)
-{
-  strcpy(config.schemapath, WJEString(configjson, "schemapath", WJE_GET, DEFAULTSCHEMAPATH));
-}
+//void parseconfig(WJElement configjson)
+//{
+//  strcpy(config.schemapath, WJEString(configjson, "schemapath", WJE_GET, DEFAULTSCHEMAPATH));
+//}
 
 int readconfig(void)
 {
-  FILE *configfile;
-  WJReader configread;
-  WJElement configjson = NULL;
-  if ((configfile = fopen(CONFIGPATH, "r")) && (configread = WJROpenFILEDocument(configfile, NULL, 0)))
-    configjson = WJEOpenDocument(configread, NULL, NULL, NULL);
+  json_object * configjson = NULL;
+  int configfile = open("test.json", O_RDONLY);
+  if (configfile != -1)
+  {
+    struct stat st;
+    fstat(configfile, &st);
+    char *buffer = malloc(st.st_size);
+    read(jsonfile, buffer, st.st_size);
+    configjson = json_tokener_parse(buffer);
+  }
   else
-    configjson = WJEObject(NULL, NULL, WJE_NEW);
-
-  parseconfig(configjson);
+  {
+    configjson = json_object_new_object();
+  }
+//  if ((configfile = fopen(CONFIGPATH, "r")) && (configread = WJROpenFILEDocument(configfile, NULL, 0)))
+//    configjson = WJEOpenDocument(configread, NULL, NULL, NULL);
+//  else
+//    configjson = WJEObject(NULL, NULL, WJE_NEW);
+//
+//  parseconfig(configjson);
 }
-
 
