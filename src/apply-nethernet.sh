26776858acf30b4e627a7ed7d17fb1560ab7539c
Art Navsegda <artnavsegda@gmail.com>
2019-10-09 12:33:22 +0300

    lol

--- src/apply-nethernet.sh
+++ src/apply-nethernet.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-#env
-#SETARRAY=`cat`
-#echo $SETARRAY
-#ssh root@192.168.1.1 stdin2arg /bin/ubus call uci set
-#ssh root@192.168.1.1 ifup $FACE
-
 ssh root@192.168.1.1 /root/apply-ethernet.sh $FACE
+ssh root@192.168.1.1 ifup $FACE
 
