16e63f92e7577bd7afd929ff3a43877097e7970f
Art Navsegda <artnavsegda@gmail.com>
2019-10-11 12:04:55 +0300

    all of this

--- src/zgen.c
+++ src/zgen.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <wjelement.h>
+#include <wjreader.h>
+#include "generator.h"
+#include "config.h"
+#include "load.h"
 
-int main()
+WJElement root = NULL;
+
+int main(int argc, char *argv[])
 {
+	if (argc < 2)
+	{
+		puts("syntax: zfilter schema.path");
+		return 1;
+	}
+	
 	return 0;
 }
