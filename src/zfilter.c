27617ad4918396ef83938a3790fb235e5085c7f4
Art Navsegda <artnavsegda@gmail.com>
2019-10-11 10:46:17 +0300

    filtor

--- src/zfilter.c
+++ src/zfilter.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <wjelement.h>
+#include <wjreader.h>
+#include "filter.h"
+#include "config.h"
+#include "load.h"
 
-int main()
+WJElement root = NULL;
+
+int main(int argc, char *argv[])
 {
+	if (argc < 2)
+	{
+		puts("syntax: zfilter schema.path");
+		return 1;
+	}
+
+	root = WJEObject(NULL, NULL, WJE_NEW);
+	readconfig();
+	loadeveryschema(root,config.schemapath);
+	WJReader readjson;
+	if (!(readjson = WJROpenFILEDocument(stdin, NULL, 0))) {
+    puts("json failed to open");
+    return 1;
+  }
+
+	//WJElement input = WJEOpenDocument(readjson, NULL, NULL, NULL);
+	//WJEDump(input);
+	//WJElement doc = filter(input,root, argv[1]);
+	//(doc);
+
+	WJEDump(filter(WJEOpenDocument(readjson, NULL, NULL, NULL),root, argv[1]));
+
 	return 0;
 }
