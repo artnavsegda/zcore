b69c6362cff3efd3794959565441802831173c82
Art Navsegda <artnavsegda@gmail.com>
2019-10-11 12:39:02 +0300

    ne mozhet

--- src/zgen.c
+++ src/zgen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <wjelement.h>
 #include <wjreader.h>
 #include "generator.h"
@@ -11,9 +12,29 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		puts("syntax: zfilter schema.path");
+		puts("syntax: zgen -o name=value -o name=value schema.path");
 		return 1;
 	}
+
+  char * values[100];
+  int valindex = 0;
+  int opt;
+
+  while ((opt = getopt(argc, argv, "o:")) != -1)
+  {
+    switch (opt)
+    {
+      case 'o':
+        values[valindex++] = optarg;
+        break;
+    }
+  }
+
+  for (int i = 0; i < valindex; i++)
+  {
+    puts(values[i]);
+  }
+
 	
 	return 0;
 }
