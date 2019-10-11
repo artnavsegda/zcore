0355f1ca2f0c6b24bb6218fd047a320ab33cebe2
Art Navsegda <artnavsegda@gmail.com>
2019-10-11 12:53:15 +0300

    zgen

--- src/zgen.c
+++ src/zgen.c
@@ -40,6 +40,12 @@ int main(int argc, char *argv[])
     puts("syntax: zgen -o name=value -o name=value schema.path");
   }
 
+  root = WJEObject(NULL, NULL, WJE_NEW);
+  readconfig();
+  loadeveryschema(root,config.schemapath);
+
+  WJElement doc = NULL;
+  doc = generator(root, schema, values);
 	
 	return 0;
 }
