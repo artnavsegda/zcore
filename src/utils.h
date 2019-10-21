44f9c31959c4936d6c8bc394d4f65851e8b4ad5e
Art Navsegda <artnavsegda@gmail.com>
2019-10-21 11:50:11 +0300

    utils

--- src/utils.h
+++ src/utils.h
@@ -4,8 +4,8 @@
 // char *strmbtok ( char *input, char *delimit, char *openblock, char *closeblock);
 // WJElement getelementbynameprop(WJElement container, char * text);
 // char * cutquot(char * stringtocut);
-// int arrlength(char **array);
-// int parse(char * stringtoparse, char **tokarr);
+int arrlength(char **array);
+int parse(char * stringtoparse, char **tokarr);
 // int ifacefound(char * ifacetosearch);
 // int commandfound(char * commandtosearch);
 // char * formatcommand(char * command);
