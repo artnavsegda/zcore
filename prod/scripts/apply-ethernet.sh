e9e77c7fce28f626e294110f2e1218109f605b37
Art Navsegda <artnavsegda@gmail.com>
2019-10-24 16:56:06 +0300

    sectio

--- prod/scripts/apply-ethernet.sh
+++ prod/scripts/apply-ethernet.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-echo $1 $2 $3
-echo sobake
-echo $@
-
 SETARRAY=`cat`
-ubus call uci set "{\"config\":\"network\", \"section\":\"$FACE\", \"type\":\"interface\", \"values\":$SETARRAY }"
-ifup $FACE
+ubus call uci set "{\"config\":\"network\", \"section\":\"$SECTION\", \"type\":\"interface\", \"values\":$SETARRAY }"
+ifup $SECTION
 
