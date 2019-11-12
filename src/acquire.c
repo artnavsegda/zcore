ac5571bf98a56ecfc14b8c3013662f17260fd820
Art Navsegda <artnavsegda@gmail.com>
2019-11-12 12:10:33 +0300

    aaaaa

--- src/acquire.c
+++ src/acquire.c
@@ -18,32 +18,40 @@ int acquire(WJElement proto)
     WJECloseDocument(data);
   }
 
+  printf("acquire %s\n", proto->name);
+
   if (WJEGet(proto, "schema.acquire.file", NULL))
   {
+    char * pathtoload = WJEString(proto, "schema.acquire.file", WJE_GET, NULL);
+    if (!(jsonstream = fopen(pathtoload, "r"))) {
+      printf("cannot open json file %s\n", pathtoload);
+      return 1;
+    }
+
     // do something
   }
   else if (WJEGet(proto, "schema.acquire.shell", NULL))
   {
-    printf("acquire %s\n", proto->name);
     //puts(WJEString(proto, "schema.acquire.shell", WJE_GET, "/bin/true"));
 
     if (!(jsonstream = my_popen_read(WJEString(proto, "schema.acquire.shell", WJE_GET, "/bin/true"), argv,  NULL, &forkpid))) {
       puts("handle error");
       return 1;
     }
-
-    if (!(readjson = WJROpenFILEDocument(jsonstream, NULL, 0))) {
-      puts("json failed to open");
-      return 1;
-    }
-
-    WJElement jsondata = WJEOpenDocument(readjson, NULL, NULL, NULL);
-    WJERename(jsondata,"data");
-    WJEAttach(proto,jsondata);
-
-    fclose(jsonstream);
-    waitpid(forkpid, &status, 0);
   }
+
+  if (!(readjson = WJROpenFILEDocument(jsonstream, NULL, 0))) {
+    puts("json failed to open");
+    return 1;
+  }
+
+  WJElement jsondata = WJEOpenDocument(readjson, NULL, NULL, NULL);
+  WJERename(jsondata,"data");
+  WJEAttach(proto,jsondata);
+
+  fclose(jsonstream);
+  if (forkpid)
+    waitpid(forkpid, &status, 0);
 
 //  WJEDump(jsondata);
   return 0;
