56f8f090bc6b0eeda9c39f6903064d62dc277477
Art Navsegda <artnavsegda@gmail.com>
2019-11-14 12:08:07 +0300

    script search path

--- src/config.h
+++ src/config.h
@@ -1,6 +1,7 @@
 #define CONFIGPATH "/etc/zenith/zenith.json"
 #define MAXPATH 255
 #define DEFAULTSCHEMAPATH "/etc/zenith/schema"
+#define DEFAULTSCRIPTPATH "/etc/zenith/script"
 #define SCHEMAEXTENSION ".schema.json"
 
 struct configstruct_t
