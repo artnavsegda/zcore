af5840cbe5b11b99a5abd279cc6d005221e13771
Art Navsegda <artnavsegda@gmail.com>
2019-11-15 15:28:37 +0300

    apply add

---
+++ prod/script/add.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+. /usr/share/libubox/jshn.sh
+
+subsystem=$1; shift
+schema=$1; shift
+
+pp=""
+    for key in $CUE; do
+        [ -z "$1" ] && break
+        val=$1; shift
+        [ -z "$val" ] && continue
+        pp="$pp -o $key=$val"
+    done
+
+logger -t "zcore-add" "$subsystem - zgen $pp $schema"
+
+DATA=$(zgen $pp $schema)
+
+json_load "$DATA" || {
+    echo "DATA corrupted"
+    exit 1
+}
+
+case "$subsystem" in
+    "apn-profiles")
+        json_get_vars $CUE
+        for key in $CUE; do
+            [ -z "$(eval echo \$$key)" ] && {
+                echo "usage: add <$CUE>"
+                exit 1
+            }
+        done
+
+        config='{"config": "apn-profiles", "type": "apn", "values": '$(json_dump)'}'
+        eval "ubus call uci add '$config'" &> /dev/null
+
+        ;;
+esac
--- prod/script/apply.sh
+++ prod/script/apply.sh
@@ -2,17 +2,22 @@
 
 SETARRAY=$(cat)
 
-case "$1" in
-  "ntp")
-    ubus call uci set "{'config':'system', 'section':'ntp', 'values':$SETARRAY }"
-  ;;
-esac
+subsystem=$1
+schema=$2
 
-#subsystem=$1
-#schema=$2
+logger -t "zenith" "apply: $@ | $SETARRAY"
 
-#logger -t "zenith" "apply: $@ | $SETARRAY"
-
-#uci -q delete network.$1
-#uci -q set network.$1=interface
 #ubus call uci set "{\"config\":\"network\", \"section\":\"$1\", \"type\":\"interface\", \"values\":$SETARRAY }"
+
+case "$subsystem" in
+    "apn-profiles")
+            ## unnamed section
+            uci -q delete apn-profiles.$SECTION ## need remove option value
+            SECTION=$(uci add apn-profiles apn)
+            DATA='{"config": "apn-profiles", "section": "'$SECTION'", "type": "apn", "values": '$SETARRAY'}'
+            eval "ubus call uci set '$DATA'"
+        ;;
+    "ntp")
+            ubus call uci set "{'config':'system', 'section':'ntp', 'values':$SETARRAY }"
+        ;;
+esac
