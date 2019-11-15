e3886f335f7f9a008eb452242c06ba0677995b47
Art Navsegda <artnavsegda@gmail.com>
2019-11-15 13:59:14 +0300

    aaaaa

--- prod/script/apply.sh
+++ prod/script/apply.sh
@@ -2,13 +2,17 @@
 
 SETARRAY=$(cat)
 
-subsystem=$1
-schema=$2
+case "$1" in
+  "ntp")
+    ubus call uci set "{'config':'system', 'section':'ntp', 'values':$SETARRAY }"
+  ;;
+esac
+
+#subsystem=$1
+#schema=$2
 
 #logger -t "zenith" "apply: $@ | $SETARRAY"
 
 #uci -q delete network.$1
 #uci -q set network.$1=interface
 #ubus call uci set "{\"config\":\"network\", \"section\":\"$1\", \"type\":\"interface\", \"values\":$SETARRAY }"
-
-
