ebbedb8bb7cb1700f5b4b430b84ed44d4c1bcf43
Art Navsegda <artnavsegda@gmail.com>
2019-11-28 10:08:01 +0300

    global

--- src/global.c
+++ src/global.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <wjelement.h>
 
+struct globalcommand_struct
+{
+  char *commandname;
+  WJElement * schemalink;
+} * globalcommands = NULL;
+
 void addglobalcommand(WJElement loadroot, char * commandname)
 {
   printf("add global command %s\n", commandname);
+
+//  for (int i = 0, globalcommands
+
+  globalcommands = realloc(globalcommands, sizeof(struct globalcommand_struct));
+  globalcommands->commandname = commandname;
 }
 
