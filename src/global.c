4def802d7f565230aface73ab78b28b74898336a
Art Navsegda <artnavsegda@gmail.com>
2019-11-28 11:14:12 +0300

    global commands

--- src/global.c
+++ src/global.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <wjelement.h>
+#include <string.h>
 
 struct globalcommand_struct
 {
   char *commandname;
+  int schemacount;
   WJElement * schemalink;
 } * globalcommands = NULL;
+
+int globalcommandcount = 0;
 
 void addglobalcommand(WJElement loadroot, char * commandname)
 {
   printf("add global command %s\n", commandname);
 
-//  for (int i = 0, globalcommands
+  for (int i = 0; i < globalcommandcount; i++)
+  {
+    if (strcmp(commandname,globalcommands[i].commandname) == 0)
+    {
+      puts("add schema to existing command");
+      globalcommands[i].schemacount++;
+      globalcommands[i].schemalink = realloc(globalcommands[i].schemalink, sizeof(WJElement *)*globalcommands[i].schemacount);
+      globalcommands[globalcommandcount-1].schemalink[globalcommands[i].schemacount-1] = loadroot;
+      return;
+    } 
+  }
 
-  globalcommands = realloc(globalcommands, sizeof(struct globalcommand_struct));
-  globalcommands->commandname = commandname;
+  puts("add new command");
+  globalcommandcount++;
+  globalcommands = realloc(globalcommands, sizeof(struct globalcommand_struct)*(globalcommandcount));
+  globalcommands[globalcommandcount-1].commandname = commandname;
+  globalcommands[globalcommandcount-1].schemalink = malloc(sizeof(WJElement *));
+  globalcommands[globalcommandcount-1].schemalink[0] = loadroot;
+  globalcommands[globalcommandcount-1].schemacount = 1;
 }
 
