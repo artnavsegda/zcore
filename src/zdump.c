b809527ac0d776dae884f00123925e287e59d411
Art Navsegda <artnavsegda@gmail.com>
2019-11-29 10:46:33 +0300

    zdump

--- src/zdump.c
+++ src/zdump.c
@@ -11,18 +11,20 @@ WJElement root = NULL;
 int main(int argc, char *argv[])
 {
   int opt;
+  char *zcoreconfig = NULL;
 
-  while ((opt = getopt(argc, argv, "o:")) != -1)
+  while ((opt = getopt(argc, argv, "c:")) != -1)
   {
     switch (opt)
     {
-      case 'o':
+      case 'c':
+        zcoreconfig = optarg;
         break;
     }
   }
 
   root = WJEObject(NULL, NULL, WJE_NEW);
-  readconfig(NULL);
+  readconfig(zcoreconfig);
   loadeveryschema(root,config.schemapath);
 
   WJEDump(root);
