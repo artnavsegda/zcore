5cd12210dbcff57a718555bf7d83d1c59844718d
Art Navsegda <artnavsegda@gmail.com>
2019-12-16 10:02:05 +0300

    test framework

--- src/test/test.sh
+++ src/test/test.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-../zcore -c $PWD/testconfig.json test.script
+# test 1
+../zcore -c $PWD/testconfig.json test1.script > test1.result
+if diff test1.result test1.reference; then
+  echo "test 1 success"
+else
+  echo "test 1 fail"
+fi
+# test1 cleanup
+rm test1.result
+
