153fd315f51e6b7236e6d936f4e748726d983827
Art Navsegda <artnavsegda@gmail.com>
2020-03-11 21:56:41 +0300

    aaa

--- src/option.c
+++ src/option.c
@@ -73,7 +73,7 @@ WJElement anyoption(WJElement schema, WJElement face)
     WJEAttach(tempschema, tempdefs);
     if (WJESchemaValidate(tempschema, face, schema_errorq, schema_load, schema_free, "%s"))
     {
-      WJEMergeObjects(optionlisttwo, myanyoption, TRUE);
+      WJEMergeObjects(optionlisttwo, myanyoption, TRUE); // TAKAYA SUKA ZASADA
     }
     WJECloseDocument(tempschema);
   }
